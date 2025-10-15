0b7ae58d34e0943543a94ff505e7f39557e12c5f
BAXENdev <83290629+BAXENdev@users.noreply.github.com>
2025-10-15 11:05:13 -0400

    Updated models to be standardized, fixed foreign key tags, and updated DB Context

---
+++ Pangolivia.API/Api/Models/PlayerGameRecordModel.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Pangolivia.API.Models;
+
+[Table("player_game_records")]
+public class PlayerGameRecordModel
+{
+
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Column("id")]
+    public int Id { get; set; }
+
+    [Required]
+    [Column("user_id")]
+    public int UserId { get; set; }
+    [ForeignKey(nameof(UserId))]
+    public UserModel? User { get; set; }
+
+    [Required]
+    [Column("game_record_id")]
+    public int GameRecordId { get; set; }
+    [ForeignKey(nameof(GameRecordId))]
+    public GameRecordModel? GameRecord { get; set; }
+
+    [Required]
+    public double score { get; set; } = 0;
+}
--- Pangolivia.API/Api/Models/Usermodel.cs
+++ Pangolivia.API/Api/Models/Usermodel.cs
@@ -1,30 +1,24 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Pangolivia.Models
+namespace Pangolivia.API.Models;
+
+[Table("users")]
+public class UserModel
 {
-    [Table("Users")]
-    public class User
-    {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int Id { get; set; }
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
 
-        [Required]
-        [Column("auth_uuid")]
-        [MaxLength(255)]
-        public string AuthUuid { get; set; } = string.Empty;
+    [Required]
+    [Column("auth_uuid")]
+    public string AuthUuid { get; set; } = string.Empty;
 
-        [Required]
-        [Column("username")]
-        [MaxLength(100)]
-        public string Username { get; set; } = string.Empty;
+    [Required]
+    [Column("username")]
+    public string Username { get; set; } = string.Empty;
 
-        // Timestamps for auditing (Just in Case)
-        [Column("created_at")]
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-
-        [Column("updated_at")]
-        public DateTime? UpdatedAt { get; set; }
-    }
+    // public ICollection<PlayerGameRecordModel> PlayerGameRecords { get; set; } = new List<PlayerGameRecordModel>();
+    // public ICollection<GameRecordModel> HostedGameRecords { get; set; } = new List<GameRecordModel>();
+    // public ICollection<QuizModel> CreatedQuizzes { get; set; } = new List<QuizModel>();
 }
