c701d7c280782c3aae28f50765c13e9c7d882a9a
Husan Nizom <husan.dev@h-n.tech>
2025-10-17 17:36:54 -0400

    updated UserModel to contain hashedPassword and implemented simple jwt login

--- Pangolivia.API/Api/Repositories/Interfaces/IUserRepository.cs
+++ Pangolivia.API/Api/Repositories/Interfaces/IUserRepository.cs
@@ -6,8 +6,8 @@ public interface IUserRepository
 {
     public Task<List<UserModel>> getAllUserModels();
     public Task<UserModel> getUserModelById(int id);
-    public Task<UserModel> getUserModelByUsername(string username);
-    public Task<UserModel> createUserModel(CreateUserDTO userDTO);
+    public Task<UserModel?> getUserModelByUsername(string username);
+    public Task<UserModel> createUserModel(UserModel user);
     public Task<UserModel> updateUserModelPlayerGameRecord(int id, PlayerGameRecordDto PGR);
     public Task<UserModel> updateUserModelHostedGameRecord(int id, GameRecordModel GRM);
     public Task<UserModel> updateUserModelCreatedQuizzes(int id, QuizModel quiz);
