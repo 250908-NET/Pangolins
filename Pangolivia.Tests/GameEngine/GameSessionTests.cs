ae4205290c09fabfdfed4d5162343d5847e59062
Danny Scally <44758401+dascally@users.noreply.github.com>
2025-10-20 14:14:17 -0400

    test(GameSession): Add HasGameStarted and RegisterPlayer tests

--- Pangolivia.Tests/GameEngine/GameSessionTests.cs
+++ Pangolivia.Tests/GameEngine/GameSessionTests.cs
@@ -1,3 +1,4 @@
+using Pangolivia.API.DTOs;
 using Pangolivia.API.GameEngine;
 
 namespace Pangolivia.Tests.GameEngine;
@@ -32,5 +33,70 @@ public class GameSessionTests
     public void Constructor_ThrowsWhenQuizIsNull()
     {
         Assert.Throws<ArgumentNullException>(() => new GameSession(id: 1, name: "name", hostUserId: 1, quiz: null!));
+    }
+
+    [Fact]
+    public void HasGameStarted_ReturnsFalse_WhenNoQuestionStarted()
+    {
+        var quiz = new QuizModel { Id = 1, Questions = new List<QuestionModel>() };
+        var session = new GameSession(id: 1, name: "s", hostUserId: 1, quiz: quiz);
+
+        Assert.False(session.HasGameStarted());
+    }
+
+    [Fact]
+    public void HasGameStarted_ReturnsTrue_WhenGameStarted()
+    {
+        // Arrange
+        var question = new QuestionModel
+        {
+            Id = 11,
+            QuizId = 1,
+            QuestionText = "What is 2+2?",
+            CorrectAnswer = "4",
+            Answer2 = "3",
+            Answer3 = "5",
+            Answer4 = "22",
+        };
+        var quiz = new QuizModel { Id = 1, Questions = new List<QuestionModel> { question } };
+        var session = new GameSession(id: 1, name: "s", hostUserId: 1, quiz: quiz);
+        Assert.True(session.HasNextQuestion());
+
+        // Act
+        var q = session.AdvanceQuestion();
+
+        // Assert
+        Assert.True(session.HasGameStarted());
+        Assert.NotNull(q);
+        Assert.Equal(question.QuestionText, q.QuestionText);
+        Assert.False(session.HasNextQuestion());
+    }
+
+    [Fact]
+    public void RegisterPlayer_AddsPlayerSuccessfully()
+    {
+        // Arrange
+        var quiz = new QuizModel { Id = 1, Questions = new List<QuestionModel>() };
+        var session = new GameSession(id: 1, name: "s", hostUserId: 1, quiz: quiz);
+        var user = new UserDto { Id = 7, Username = "player7" };
+
+        // Act
+        var returned = session.RegisterPlayer(user, connectionId: "conn-1");
+
+        // Assert
+        Assert.Same(user, returned);
+    }
+
+    [Fact]
+    public void RegisterPlayer_ThrowsWhenDuplicate()
+    {
+        // Arrange
+        var quiz = new QuizModel { Id = 1, Questions = new List<QuestionModel>() };
+        var session = new GameSession(id: 1, name: "s", hostUserId: 1, quiz: quiz);
+        var user = new UserDto { Id = 7, Username = "player7" };
+        session.RegisterPlayer(user, connectionId: "conn-1");
+
+        // Act & Assert
+        Assert.Throws<InvalidOperationException>(() => session.RegisterPlayer(user, connectionId: "conn-2"));
     }
 }
