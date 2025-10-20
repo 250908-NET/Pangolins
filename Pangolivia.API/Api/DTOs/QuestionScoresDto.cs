4b51ec70fea11ed9f33cf13bf6ab90f429e3d415
Danny Scally <44758401+dascally@users.noreply.github.com>
2025-10-20 10:20:21 -0400

    feat: Implement QuestionScoresDto

---
+++ Pangolivia.API/Api/DTOs/QuestionScoresDto.cs
@@ -0,0 +1,15 @@
+namespace Pangolivia.API.DTOs;
+
+public record QuestionScoresDto
+{
+    public required string Question { get; init; }
+    public required string Answer { get; init; }
+    public List<PlayerQuestionScoresDto> PlayerScores { get; init; } = new();
+}
+
+public record PlayerQuestionScoresDto
+{
+    public int UserId { get; init; }
+    public required string Username { get; init; }
+    public double Score { get; init; }
+}
