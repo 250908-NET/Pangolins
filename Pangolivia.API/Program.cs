f04f18087b750d54a11397f24c374d79b132dc4e
baxendev <brewer01business@gmail.com>
2025-10-28 02:16:36 +0000

    Removed https redirection since we don't handle that in docker

--- Pangolivia.API/Program.cs
+++ Pangolivia.API/Program.cs
@@ -174,7 +174,7 @@ if (app.Environment.IsDevelopment())
     app.UseMiddleware<RequestLoggingMiddleware>();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
 app.UseAuthentication();
 app.UseAuthorization();
